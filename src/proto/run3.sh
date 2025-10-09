18d63861955c99564c236914d1926ff1b45dc6b7
Chinmay Mhatre <chinmaymhatre111@gmail.com>
2025-10-09 06:55:20 +0530

    feat: fix grpc for python3

--- src/proto/run3.sh
+++ src/proto/run3.sh
@@ -1,2 +1,2 @@
-python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. ./client.proto
-sed -i 's/client_pb2/proto.client_pb2/1' client_pb2_grpc.py
+python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. ./grpc.proto
+sed -i '' 's/grpc_pb2/proto.grpc_pb2/1' grpc_pb2_grpc.py
